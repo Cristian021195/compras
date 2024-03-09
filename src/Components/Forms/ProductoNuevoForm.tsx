063b0e3ba538c6c1499416d0f2248ad73816098c
Cristian Ismael Gramajo <cristiangramajo015@gmail.com>
2024-03-09 00:52:30 -0300

    cambios

--- src/Components/Forms/ProductoNuevoForm.tsx
+++ src/Components/Forms/ProductoNuevoForm.tsx
@@ -24,6 +24,7 @@ export const ProductoNuevoForm = () => {
     const [filtrados, setFiltrados] = useState<TProducto[]>([]);
     const [alerta,setAlerta] = useState(false);
     const [promptAlert,setPromptAlert] = useState(false);
+    const [promptDb,setPromptDb] = useState(false);
     const [alertaDetalle, setAlertaDetalle] = useState({});
 
     const nombre = useRef<HTMLInputElement>(null);
@@ -39,6 +40,24 @@ export const ProductoNuevoForm = () => {
             setFiltrados(prod);
         }else{
             nombre.current?.focus();
+        }
+    }
+
+    const compartir = async () => {
+        let prods = await db.productos.toArray(pr=>pr.map(pm=>{return {nombre:pm.nombre, precio:pm.precio}}));
+        let formated = prods.map(fp=>`${fp.nombre}=${fp.precio}`);
+        let str = formated.join('\n');
+
+        let myData = {title: 'Capo',text: str}
+        if (navigator.canShare(myData)) {
+            await navigator.share(myData);
+        }
+    }
+
+    const compar = async () => {
+        let myData = {title: 'My title',text: 'My text'}
+        if (navigator.canShare(myData)) {
+            await navigator.share(myData);
         }
     }
 
@@ -159,13 +178,15 @@ export const ProductoNuevoForm = () => {
         <button type="button" className='btn text-w m-1 p-1' style={{backgroundColor:'dodgerblue'}} onClick={buscar}>
             <Search width={14} height={14}/>
         </button>
-        <button type="button" className='btn text-w m-1 p-1' style={{backgroundColor:'gainsboro', color:'black'}} onClick={()=>{}}>
+        <button type="button" className='btn text-w m-1 p-1' style={{backgroundColor:'gainsboro', color:'black'}} onClick={
+            compar
+        }>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
               <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5"/>
               <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708z"/>
             </svg>
         </button>
-        <button type="button" className='btn text-w m-1 p-1' style={{backgroundColor:'gold', color:'black'}} onClick={()=>{}}>
+        <button type="button" className='btn text-w m-1 p-1' style={{backgroundColor:'gold', color:'black'}} onClick={compartir}>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 18 16">
                 <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.5 2.5 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5"/>
             </svg>
--- src/Components/UI/Prompt.tsx
+++ src/Components/UI/Prompt.tsx
@@ -12,6 +12,7 @@ interface IPrompt{
     timeout?:number,
     onCancel?:()=>void,
     onConfirm?:()=>void,
+    onAlternative?:()=>void,
     children?:React.ReactNode,
 };
 
@@ -25,10 +26,8 @@ text="¿Ejecutar Acción?", status=false, children,timeout=3000, copied=false, c
         }
     },[]);
     return (
-        <div style={{border:'solid 0.3rem '+borderColor,minHeight:'100%',width:'100%', top: '50%', left: '50%',  transform: 'translate(-50%, -50%)',
-            position:'fixed', backgroundColor:'rgba(0,0,0,0.2)', backdropFilter:'blur(1px)', zIndex:2}}
-            className={"p-2 d-flex justify-content-center align-items-center "+cssClass}>
-            <div style={{backgroundColor:bgcolor, padding:'1rem', borderRadius:'0.3rem'}}>
+        <div className={"p-2 d-flex justify-content-center align-items-center prompt "+cssClass}>
+            <div className="prompt-in">
                 <div>
                     <h3>{title}</h3>
                     <p>{text}</p>
@@ -39,10 +38,48 @@ text="¿Ejecutar Acción?", status=false, children,timeout=3000, copied=false, c
                     </div>
                 </div>
                 <div>
-                    <button type="button" className='btn text-w m-1 p-1' style={{backgroundColor:'#66BB6A'}}
+                    <button type="button" className='btn text-w m-1 p-1 c-green' 
                         onClick={onConfirm}
                     >Aceptar</button>
-                    <button type="button" className='btn text-w m-1 p-1' style={{backgroundColor:'#e6744e'}}
+                    <button type="button" className='btn text-w m-1 p-1 c-main' 
+                        onClick={onCancel}
+                    >Cancelar</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export const PromptDouble = ({color="#f5f5f5", bgcolor="#f5f5f5", title="Accion",borderColor="#f5f5f5",
+text="¿Ejecutar Acción?", status=false, children,timeout=3000, copied=false, cssClass="", onCancel,onConfirm ,onAlternative}:IPrompt) => {
+    const [hide, setHide] = useState<boolean | undefined>(status);
+    useEffect(()=>{
+        document.body.style.overflowY = "hidden";
+        return () => {
+            document.body.style.overflowY = "scroll";
+        }
+    },[]);
+    return (
+        <div className={"p-2 d-flex justify-content-center align-items-center prompt "+cssClass}>
+            <div className="prompt-in">
+                <div>
+                    <h3>{title}</h3>
+                    <p>{text}</p>
+                </div>
+                <div>
+                    <div>
+                        {children}
+                    </div>
+                </div>
+                <div>
+                    <button type="button" className='btn text-w m-1 p-1 c-green'
+                        onClick={onAlternative}
+                    >Descargar</button>
+                    <button type="button" className='btn text-w m-1 p-1 c-green'
+                        onClick={onConfirm}
+                    >Compartir</button>
+                    <button type="button" className='btn text-w m-1 p-1 c-main'
                         onClick={onCancel}
                     >Cancelar</button>
                 </div>
